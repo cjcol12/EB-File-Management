d1b4e68cde4be9c4c16c99e7e66db7a7781c7afa
cjcol12 <cj2014999@hotmail.com>
2023-02-13 14:50:32 +0000

    image struct initialised

--- ebfEcho.c
+++ ebfEcho.c
@@ -48,11 +48,11 @@ int main(int argc, char **argv)
         } // check file pointer
 
     // get first 2 characters which should be magic number
-    magicNumber[0] = getc(inputFile);
-    magicNumber[1] = getc(inputFile);
+    image_struct.magicNumber[0] = getc(inputFile);
+    image_struct.magicNumber[1] = getc(inputFile);
 
     // checking against the casted value due to endienness.
-    if (*magicNumberValue != MAGIC_NUMBER)
+    if (image_struct.magicNumberValue != MAGIC_NUMBER)
         { // check magic number
         printf("ERROR: Bad Magic Number\n");
         return BAD_FILE;
@@ -60,8 +60,8 @@ int main(int argc, char **argv)
 
     // scan for the dimensions
     // and capture fscanfs return to ensure we got 2 values.
-    int check = fscanf(inputFile, "%d %d", &height, &width);
-    if (check != 2 || height < MIN_DIMENSION || width < MIN_DIMENSION || height > MAX_DIMENSION || width > MAX_DIMENSION)
+    int check = fscanf(inputFile, "%d %d", &image_struct.height, &image_struct.width);
+    if (check != 2 || image_struct.height < MIN_DIMENSION || image_struct.width < MIN_DIMENSION || image_struct.height > MAX_DIMENSION || image_struct.width > MAX_DIMENSION)
         { // check dimensions
         // close the file as soon as an error is found
         fclose(inputFile);
@@ -71,11 +71,11 @@ int main(int argc, char **argv)
         } // check dimensions
 
     // caclulate total size and allocate memory for array
-    numBytes = height * width;
-    imageData = (unsigned int *)malloc(numBytes * sizeof(unsigned int));
+    image_struct.numBytes = image_struct.height * image_struct.width;
+    image_struct.imageData = (unsigned int *)malloc(image_struct.numBytes * sizeof(unsigned int));
 
     // if malloc is unsuccessful, it will return a null pointer
-    if (imageData == NULL)
+    if (image_struct.imageData == NULL)
         { // check malloc
         fclose(inputFile);
         printf("ERROR: Malloc Failed\n");
@@ -83,14 +83,14 @@ int main(int argc, char **argv)
         } // check malloc
 
     // read in each grey value from the file
-    for (int current = 0; current < numBytes; current++)
+    for (int current = 0; current < image_struct.numBytes; current++)
         { // reading in
-        check = fscanf(inputFile, "%u", &imageData[current]);
+        check = fscanf(inputFile, "%u", &image_struct.imageData[current]);
         // validate that we have captured 1 pixel value
         if (check != 1)
             { // check inputted data
             // ensure that allocated data is freed before exit.
-            free(imageData);
+            free(image_struct.imageData);
             fclose(inputFile);
             printf("ERROR: Bad Data\n");
             return BAD_DATA;
@@ -105,38 +105,38 @@ int main(int argc, char **argv)
     // validate that the file has been opened correctly
     if (outputFile == NULL)
         { // validate output file
-        free(imageData);
+        free(image_struct.imageData);
         printf("ERROR: Bad File Name\n");
         return BAD_FILE;
         } // validate output file
 
     // write the header data in one block
-    check = fprintf(outputFile, "eb\n%d %d\n", height, width);
+    check = fprintf(outputFile, "eb\n%d %d\n", image_struct.height, image_struct.width);
     // and use the return from fprintf to check that we wrote.
     if (check == 0) 
         { // check write
         fclose(outputFile);
-        free(imageData);
+        free(image_struct.imageData);
         printf("ERROR: Bad Output\n");
         return BAD_OUTPUT;
         } // check write
 
     // iterate though the array and print out pixel values
-    for (int current = 0; current < numBytes; current++)
+    for (int current = 0; current < image_struct.numBytes; current++)
         { // writing out
         // if we are at the end of a row ((current+1)%width == 0) then write a newline, otherwise a space.
-        check = fprintf(inputFile, "%u%c", imageData[current], ((current + 1) % width) ? ' ' : '\n');
+        check = fprintf(inputFile, "%u%c", image_struct.imageData[current], ((current + 1) % image_struct.width) ? ' ' : '\n');
         if (check == 0)
             { // check write
             fclose(outputFile);
-            free(imageData);
+            free(image_struct.imageData);
             printf("ERROR: Bad Output\n");
             return BAD_OUTPUT;
             } // check write
         } // writing out
 
     // free allocated memory before exit
-    free(imageData);
+    free(image_struct.imageData);
     // close the output file before exit
     fclose(outputFile);
 
