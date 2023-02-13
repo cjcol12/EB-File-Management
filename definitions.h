7ad11547b0b64c6d65aa24a5d88fe2397d0742ee
cjcol12 <cj2014999@hotmail.com>
2023-02-13 14:44:56 +0000

    moved definitions to own header file

---
+++ definitions.h
@@ -0,0 +1,11 @@
+#define SUCCESS 0
+#define BAD_ARGUMENT_COUNT 1
+#define BAD_FILE 2
+#define BAD_MAGIC_NUMBER 3
+#define BAD_DIM 4
+#define BAD_MALLOC 5
+#define BAD_DATA 6
+#define BAD_OUTPUT 7
+#define MAGIC_NUMBER 0x6265
+#define MAX_DIMENSION 262144
+#define MIN_DIMENSION 1
