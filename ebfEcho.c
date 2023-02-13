254c8fbf7e1dc6bebab2bf96e2e7cfcf4a718ef2
cjcol12 <cj2014999@hotmail.com>
2023-02-13 15:35:56 +0000

    read file functions seperated, basic functionality working, output format needs work

--- ebfEcho.c
+++ ebfEcho.c
@@ -28,40 +28,41 @@ int check_arg_count(int argc){
         printf("ERROR: Bad Arguments\n");
         return BAD_ARGUMENT_COUNT;
         }
+    
+    else return 0;
 }
 
-int check_magic_number(char **argv){
+int check_file_opened(char *executable_name, FILE *inputFile){
+    if (!inputFile)
+    { // check file pointer
+    printf("ERROR: Bad File Name (%s)\n", executable_name);
+    return BAD_FILE;
+    }
+    else return 0;
+}
+
+int check_magic_number(char *executable_name, FILE *inputFile){
     image_struct.magicNumber[0] = getc(inputFile);
     image_struct.magicNumber[1] = getc(inputFile);
 
+    image_struct.magicNumberValue = (unsigned short *)image_struct.magicNumber;
+    
     // checking against the casted value due to endienness.
-    if (image_struct.magicNumberValue != MAGIC_NUMBER)
-        { // check magic number
-        printf("ERROR: Bad Magic Number\n");
+    if (*(image_struct.magicNumberValue) != MAGIC_NUMBER){
+        // check magic number
+        printf("ERROR: Bad Magic Number (%s)\n", executable_name);
         return BAD_FILE;
-        } //check magic number
+    }
+    else return 0;
+
 }
 
-int main(int argc, char **argv)
-    { // main
-
-
-
-
-    // open the input file in read mode
-    FILE *inputFile = fopen(argv[1], "r");
-    // check file opened successfully
-    if (!inputFile)
-        { // check file pointer
-        printf("ERROR: Bad File Name\n");
-        return BAD_FILE;
-        } // check file pointer
-
+int check_dimensions(char *executable_name, FILE *inputFile){
     // scan for the dimensions
     // and capture fscanfs return to ensure we got 2 values.
-    int check = fscanf(inputFile, "%d %d", &image_struct.height, &image_struct.width);
+    image_struct.check = fscanf(inputFile, "%d %d", &image_struct.height, &image_struct.width);
 
-    if (check != 2 || 
+    if (image_struct.check != 2 || 
         image_struct.height < MIN_DIMENSION || 
         image_struct.width < MIN_DIMENSION || 
         image_struct.height > MAX_DIMENSION || 
@@ -71,28 +72,34 @@ int main(int argc, char **argv)
         // close the file as soon as an error is found
         fclose(inputFile);
         // print appropriate error message and return
-        printf("ERROR: Bad Dimensions\n");
+        printf("ERROR: Bad Dimensions (%s)\n", executable_name);
         return BAD_DIM;
-        } // check dimensions
+    } // check dimensions
+    else return 0;
+}
 
+int check_malloc(FILE *inputFile){
     // caclulate total size and allocate memory for array
     image_struct.numBytes = image_struct.height * image_struct.width;
     image_struct.imageData = (unsigned int *)malloc(image_struct.numBytes * sizeof(unsigned int));
 
     // if malloc is unsuccessful, it will return a null pointer
-    if (image_struct.imageData == NULL)
-        { // check malloc
+    if (image_struct.imageData == NULL){
+        // check malloc
         fclose(inputFile);
-        printf("ERROR: Malloc Failed\n");
+        printf("ERROR: Image Malloc Failed\n");
         return BAD_MALLOC;
-        } // check malloc
+    }
+    else return 0;
+}
 
+int read_data(char executable_name, FILE *inputFile){
     // read in each grey value from the file
     for (int current = 0; current < image_struct.numBytes; current++)
         { // reading in
-        check = fscanf(inputFile, "%u", &image_struct.imageData[current]);
+        image_struct.check = fscanf(inputFile, "%u", &image_struct.imageData[current]);
         // validate that we have captured 1 pixel value
-        if (check != 1)
+        if (image_struct.check != 1)
             { // check inputted data
             // ensure that allocated data is freed before exit.
             free(image_struct.imageData);
@@ -101,6 +108,26 @@ int main(int argc, char **argv)
             return BAD_DATA;
             } // check inputted data
         } // reading in
+}
+
+int main(int argc, char **argv)
+    { // main
+
+    check_arg_count(argc);
+
+    // open the input file in read mode
+    FILE *inputFile = fopen(argv[1], "r");
+    // check file opened successfully
+    check_file_opened(argv[0], inputFile);
+
+    check_magic_number(argv[0], inputFile);
+    check_dimensions(argv[0], inputFile);
+    check_malloc(inputFile);
+    read_data(argv[0], inputFile);
+
+
+
+
 
     // now we have finished using the inputFile we should close it
     fclose(inputFile);
@@ -116,9 +143,9 @@ int main(int argc, char **argv)
         } // validate output file
 
     // write the header data in one block
-    check = fprintf(outputFile, "eb\n%d %d\n", image_struct.height, image_struct.width);
+    image_struct.check = fprintf(outputFile, "eb\n%d %d\n", image_struct.height, image_struct.width);
     // and use the return from fprintf to check that we wrote.
-    if (check == 0) 
+    if (image_struct.check == 0) 
         { // check write
         fclose(outputFile);
         free(image_struct.imageData);
@@ -130,8 +157,8 @@ int main(int argc, char **argv)
     for (int current = 0; current < image_struct.numBytes; current++)
         { // writing out
         // if we are at the end of a row ((current+1)%width == 0) then write a newline, otherwise a space.
-        check = fprintf(inputFile, "%u%c", image_struct.imageData[current], ((current + 1) % image_struct.width) ? ' ' : '\n');
-        if (check == 0)
+        image_struct.check = fprintf(inputFile, "%u%c", image_struct.imageData[current], ((current + 1) % image_struct.width) ? ' ' : '\n');
+        if (image_struct.check == 0)
             { // check write
             fclose(outputFile);
             free(image_struct.imageData);
