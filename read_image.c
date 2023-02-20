4e32d9fabe81b089432fb1f7306d5d7c28bc6871
CJ Coleman <py20cjcb@leeds.ac.uk>
2023-02-20 13:20:41 +0000

    initial file commit for read file module

---
+++ read_image.c
@@ -0,0 +1,15 @@
+// Standard I/O header file inclusion
+#include <stdio.h>
+
+// Standard library header file inclusion
+#include <stdlib.h>
+
+// Image struct header file inclusion
+#include "image_structs.h"
+
+// Definition header file inclusion
+#include "definitions.h"
+
+// image struct initialization
+// TODO Remove from global
+image_struct_type image_struct;
