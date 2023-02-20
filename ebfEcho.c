060e0eba726f86cc13764a69764eacc7eebc7b5b
CJ Coleman <py20cjcb@leeds.ac.uk>
2023-02-20 14:48:59 +0000

    path name working for error reporting

--- ebfEcho.c
+++ ebfEcho.c
@@ -13,36 +13,45 @@
  // Write image module inclusion
 #include "write_image.c"
 
+int unix_usage(int argc){
+    // Unix usage check - runs with no arguments
+    if (argc == 1){
+        printf("Usage: ebfEcho file1 file2");
+        return USAGE_REQUEST;
+    }
+
+    // return 10 on function success
+    else return 10;  // is this return value valid?
+}
 
 int main(int argc, char **argv)
-    {
+{
 
     // Unix usage information
-    if (unix_usage(argv[0], argc) == 0)
+    if (unix_usage(argc) == 0)
         return 0;
 
     // running tests on argument counts
     if (check_arg_count(argv[0], argc) == BAD_ARGUMENT_COUNT)
         return BAD_ARGUMENT_COUNT;
 
-
     // open the input file in read mode
     FILE *inputFile = fopen(argv[1], "r");
 
     // check file opened successfully
-    if (check_file_opened(argv[0], inputFile) == BAD_FILE)
+    if (check_file_opened(argv[1], inputFile) == BAD_FILE)
         return BAD_FILE;
 
-    if (check_magic_number(argv[0], inputFile) == BAD_MAGIC_NUMBER)
+    if (check_magic_number(argv[1], inputFile) == BAD_MAGIC_NUMBER)
         return BAD_MAGIC_NUMBER;
     
-    if (check_dimensions(argv[0], inputFile) == BAD_DIMENSION)
+    if (check_dimensions(argv[1], inputFile) == BAD_DIMENSION)
         return BAD_DIMENSION;
 
     if(check_malloc(inputFile) == BAD_MALLOC)
         return BAD_MALLOC;
 
-    if (read_data(argv[0], inputFile) == BAD_DATA)
+    if (read_data(argv[1], inputFile) == BAD_DATA)
         return BAD_DATA;
 
     // now we have finished using the inputFile we should close it
@@ -55,7 +64,6 @@ int main(int argc, char **argv)
         return BAD_WRITE_PERMISSIONS;
 
     // write output file header and data
-
 
     write_header(outputFile);
     write_image_data(outputFile);
