4de50abfcb704d502e812e71477b8d5d6e4e2e85
CJ Coleman <py20cjcb@leeds.ac.uk>
2023-02-20 19:08:45 +0000

    added include path (big mistake)

---
+++ compare_image.c
@@ -0,0 +1,47 @@
+// Standard I/O header file inclusion
+#include <stdio.h>
+
+// Standard library header file inclusion
+#include <stdlib.h>
+
+// Definition header file inclusion
+#include "definitions.h"
+
+// #include "image_structs.h"
+
+int comp_magic_number(image_struct_type *image_struct, image_struct_type *image_struct_compare){
+    if (*(image_struct->magicNumberValue) != *(image_struct_compare->magicNumberValue)){ // free and exit
+        free(image_struct->imageData);
+        free(image_struct->imageData);
+        printf("DIFFERENT\n");
+        return FUNCTION_SUCCESS_DIFFERENT;
+    }
+    else return FUNCTION_SUCCESS;
+}
+
+int comp_dimensions(image_struct_type *image_struct, image_struct_type *image_struct_compare){
+    if ((image_struct->height != image_struct_compare->height) || 
+        (image_struct->width != image_struct_compare->width)){
+            free(image_struct->imageData);
+            free(image_struct_compare->imageData);
+            printf("DIFFERENT\n");
+            return FUNCTION_SUCCESS_DIFFERENT;
+            } // free and exit
+    else return FUNCTION_SUCCESS;
+}
+
+int comp_image_data(image_struct_type *image_struct, image_struct_type *image_struct_compare){
+        // and check the pixel values
+    for (int n=0; n<image_struct->numBytes; n++)
+        {
+        if(image_struct->imageData[n]!=image_struct_compare->imageData[n])
+            { // free and exit
+            free(image_struct->imageData);
+            free(image_struct_compare->imageData);
+            printf("DIFFERENT\n");
+            return FUNCTION_SUCCESS_DIFFERENT;
+            } // free and exit
+        }
+    return FUNCTION_SUCCESS;
+
+}
