8d6ecd2a61d29f978613c7871dd9bcd9cdb31905
CJ Coleman <py20cjcb@leeds.ac.uk>
2023-02-22 19:06:10 +0000

    magic number written dynamically

--- write_image.c
+++ write_image.c
@@ -7,9 +7,13 @@
 // Definition header file inclusion
 #include "definitions.h"
 
+#include "image_structs.h"
+
 int write_header(image_struct_type *image_struct, FILE *outputFile){
     // write the header data in one block
-    image_struct->check = fprintf(outputFile, "eb\n%d %d\n", image_struct->height, image_struct->width);
+    // change to write with magicNumberValue
+    image_struct->check = fprintf(outputFile, "%c%c\n%d %d\n",
+    image_struct->magicNumber[0], image_struct->magicNumber[1], image_struct->height, image_struct->width);
     // and use the return from fprintf to check that we wrote.
     if (image_struct->check == 0) 
         { // check write
