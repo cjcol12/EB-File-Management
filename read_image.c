7bf9bb287e34d62160829aac64f34da76ae0650f
CJ Coleman <py20cjcb@leeds.ac.uk>
2023-02-28 17:08:13 +0000

    final(ish) binary file read/write

--- read_image.c
+++ read_image.c
@@ -177,13 +177,15 @@ int read_data(
 // add error checking
 int read_binary_data(
     image_struct_type *image_struct, char *input_file_name, FILE *input_file){
+    unsigned char test;
+    fread(&test, sizeof(unsigned char), 1, input_file); // not sure why needed
+    
     for(int i = 0; i < image_struct->height; i++){
         for(int j = 0; j < image_struct->width; j++){
             unsigned char binary_value;
             unsigned int value;
 
             image_struct->check = fread(&binary_value, sizeof(unsigned char), 1, input_file);
-            printf("binary value is %c\n", binary_value);
 
             if(image_struct->check != 1){
                 printf("ERROR: Bad Data (%s)\n", input_file_name);
@@ -192,7 +194,6 @@ int read_binary_data(
             value = (unsigned int) binary_value;
 
             image_struct->imageData[i][j] = value;
-            printf("value is %u\n", image_struct->imageData[i][j]);
 
             if (image_struct->imageData[i][j] > MAX_GRAY
             ||image_struct->imageData[i][j] < MIN_GRAY){
