6995f34c7132df9e06ce3dc1548cf8b90b922298
CJ Coleman <py20cjcb@leeds.ac.uk>
2023-03-02 13:08:17 +0000

    malloc bug finally squashed you bastard

--- read_image.c
+++ read_image.c
@@ -21,6 +21,53 @@
 // Function prototype header file inclusion
 #include "read_image.h"
 
+// void construct_image(image_struct_type *image_struct){
+//     image_struct->magic_number[0] = NULL;
+//     image_struct->magic_number_value = NULL;
+
+//     // create and initialise variables used within code
+//     image_struct->width = NULL;
+//     image_struct->height = NULL;
+//     image_struct->check = NULL;
+
+//     // image data 
+//     image_struct->imageData = NULL;
+//     image_struct->numBytes = NULL;
+// }
+
+
+// void destruct_image(image_struct_type *image_struct){
+//     if ((image_struct->magic_number[0]) != NULL)
+//         image_struct->magic_number[0] = NULL;
+    
+//     if (image_struct->magic_number[1] != NULL)
+//         image_struct->magic_number[1] = NULL;
+
+//     if (image_struct->magic_number_value != NULL)
+//         image_struct->magic_number_value = NULL;
+
+//     if (image_struct->width != NULL)
+//         image_struct->width = NULL;
+
+//     if (image_struct->height != NULL)
+//         image_struct->height = NULL;
+
+//     if (image_struct->check != NULL)
+//         image_struct->check = NULL;
+
+//     if (image_struct->imageData != NULL)
+//         image_struct->imageData = NULL;
+    
+//     if (image_struct->numBytes != NULL)
+//         image_struct->numBytes = NULL;
+
+//     if (input_file != NULL)
+//         input_file = NULL;
+    
+//     if (output_file != NULL)
+//         output_file = NULL;
+// }
+
 int check_arg_count(int argc){
     // validate that user has entered 2 arguments
     if (argc != 3){
@@ -86,26 +133,52 @@ int check_dimensions(
 }
 
 int check_malloc(image_struct_type *image_struct, FILE *input_file){
+    image_struct->numBytes = image_struct->width * image_struct->height;
     // malloc for 2d array
-    image_struct->imageData = 
-    (unsigned int **) malloc(image_struct->height * sizeof(unsigned int *));
+    image_struct->imageData = (unsigned int **) malloc(image_struct->height * sizeof(unsigned int *));
 
-    for(int i = 0; i < image_struct->height; i++){
-        image_struct->imageData[i] = 
-        (unsigned int *) malloc(image_struct->width * sizeof(unsigned int));
-
-        if (image_struct->imageData[i] == NULL){
-            fclose(input_file);
-            printf("ERROR: Image Malloc Failed\n");
-
-            for(int j = 0; j < i; j++){
-                free(image_struct->imageData[j]);
-            }
-            free(image_struct->imageData);
-            
-            return BAD_MALLOC;
-        }
+    // testing return value of malloc
+    if (image_struct->imageData == NULL){
+        fclose(input_file);
+        printf("ERROR: Image Malloc Failed\n");        
+        return BAD_MALLOC;
     }
+    
+
+    unsigned int *data_block = (unsigned int *) malloc(image_struct->numBytes * sizeof(unsigned int));
+
+    if (data_block == NULL){
+        fclose(input_file);
+        printf("ERROR: Image Malloc Failed\n");        
+        return BAD_MALLOC;
+    }
+
+    for (int row = 0; row < image_struct->height; row++){
+        image_struct->imageData[row] = data_block + row * image_struct->width; 
+    }
+
+
+
+
+
+
+
+    // for(int i = 0; i < image_struct->height; i++){
+    //     image_struct->imageData[i] = 
+    //     (unsigned int *) malloc(image_struct->width * sizeof(unsigned int));
+
+    //     if (image_struct->imageData[i] == NULL){
+    //         fclose(input_file);
+    //         printf("ERROR: Image Malloc Failed\n");
+
+    //         for(int j = 0; j < i; j++){
+    //             free(image_struct->imageData[j]);
+    //         }
+    //         free(image_struct->imageData);
+            
+    //         return BAD_MALLOC;
+    //     }
+    // }
     // if malloc is unsuccessful, it will return a null pointer
 
     // return 0 on function success
@@ -115,40 +188,40 @@ int check_malloc(image_struct_type *image_struct, FILE *input_file){
 int read_data(
     image_struct_type *image_struct, char *input_file_name, FILE *input_file){
 
-    for(int i = 0; i < image_struct->height; i++){
+    for(int i = 0; i < 360; i++){
         for(int j = 0; j < image_struct->width; j++){
             unsigned int data;
             image_struct->check = fscanf(input_file, "%u", &data);
-            image_struct->imageData[i][j] = data;
 
             if (image_struct->check != 1){
-
-                // iterate through imageData to free 2nd dimension arrays
-                for(int i = 0; i < image_struct->height; i++){
-                    free(image_struct->imageData[i]);
-                }
-                free(image_struct->imageData);
-
                 fclose(input_file);
                 printf("ERROR: Bad Data (%s)\n", input_file_name);
                 return BAD_DATA;
             }
+            
+            image_struct->imageData[i][j] = data;
+
+
+            
             // check data is within bounds (0 - 31)
             if (image_struct->imageData[i][j] > MAX_GRAY
             ||image_struct->imageData[i][j] < MIN_GRAY){
                 
                 // iterate through imageData to free 2nd dimension arrays
-                for(int i = 0; i < image_struct->height; i++){
-                    free(image_struct->imageData[i]);
-                }
-                free(image_struct->imageData);
+                // for(int i = 0; i < image_struct->height; i++){
+                //     free(image_struct->imageData[i]);
+                // }
+                // free(image_struct->imageData);
 
                 printf("ERROR: Bad Data (%s)\n", input_file_name);
                 return BAD_DATA;
             }
+
+            //printf("i is %d\n j is %d\n", i, j);
         }
     }
-
+ 
+    
     // repeat fscanf to check if there is any data we haven't read in
     image_struct->check = fscanf(input_file, "%u", *image_struct->imageData);
     // image_struct.check = 
@@ -159,10 +232,10 @@ int read_data(
     if (image_struct->check == 1){
         
         // iterate through imageData to free 2nd dimension arrays
-        for(int i = 0; i < image_struct->height; i++){
-            free(image_struct->imageData[i]);
-        }
-        free(image_struct->imageData);
+        // for(int i = 0; i < image_struct->height; i++){
+        //     free(image_struct->imageData[i]);
+        // }
+        // free(image_struct->imageData);
 
         fclose(input_file);
         printf("ERROR: Bad Data (%s)\n", input_file_name);
@@ -199,10 +272,10 @@ int read_binary_data(
             ||image_struct->imageData[i][j] < MIN_GRAY){
                 
                 // iterate through imageData to free 2nd dimension arrays
-                for(int i = 0; i < image_struct->height; i++){
-                    free(image_struct->imageData[i]);
-                }
-                free(image_struct->imageData);
+                // for(int i = 0; i < image_struct->height; i++){
+                //     free(image_struct->imageData[i]);
+                // }
+                // free(image_struct->imageData);
 
                 printf("ERROR: Bad Data (%s)\n", input_file_name);
                 return BAD_DATA;
@@ -220,10 +293,10 @@ int read_binary_data(
 int close_file_free_mem(
     image_struct_type *image_struct, char *executable_name, FILE *input_file){
     // iterate through imageData to free 2nd dimension arrays
-    for(int i = 0; i < image_struct->height; i++){
-        free(image_struct->imageData[i]);
-    }
-    free(image_struct->imageData);
+    // for(int i = 0; i < image_struct->height; i++){
+    //     free(image_struct->imageData[i]);
+    // }
+    // free(image_struct->imageData);
 
     fclose(input_file);
     printf("ERROR: Bad Data (%s)\n", executable_name);
