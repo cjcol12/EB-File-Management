e1bb41bf8d6b05b13368b602c92f28832b890ce6
CJ Coleman <py20cjcb@leeds.ac.uk>
2023-03-10 23:05:52 +0000

    error checking

--- read_image.h
+++ read_image.h
@@ -8,6 +8,7 @@ int check_file_opened(char *input_file_name, FILE *input_file);
 int check_magic_number(
     image_struct_type *image_struct, char *input_file_name, FILE *inputFile);
 
+
 int check_dimensions(
     image_struct_type *image_struct, char *input_file_name, FILE *input_file);
     
@@ -15,5 +16,9 @@ int check_malloc(image_struct_type *image_struct, FILE *input_file);
 
 int read_data(
     image_struct_type *image_struct, char *input_file_name, FILE *input_file);
+
+int check_mn_valid(image_struct_type *image_struct, FILE *input_file, char *input_file_name);
+
+int check_dimensions_valid(image_struct_type *image_struct, FILE *input_file, char *input_file_name);
 
 #endif
