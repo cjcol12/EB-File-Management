4a8ecc882675912383a456c058a2fa996c20177c
Cj Coleman <cj2014999@hotmail.com>
2023-03-21 12:41:11 +0000

    finally actually really genuinley working

--- compression.c
+++ compression.c
@@ -3,17 +3,17 @@
 #include<time.h>
 
 
-#define ROW 2
-#define COL 16
+#define ROW 10
+#define COL 72
 
 void print_binary(int num);
 
 
 unsigned int *arr[ROW];
 unsigned int **array2d = arr;
-unsigned int compressed_array[2][11] = {{0}};
+unsigned int compressed_array[ROW][45] = {{0}};
 
-unsigned int uncompressed_array[2][11] = {{0}};
+unsigned int uncompressed_array[ROW][COL] = {{0}};
 
 void fill_array_manual(){
 
@@ -28,14 +28,41 @@ void fill_array_manual(){
             array2d[i][j] = rand() % 31;
     }
     
-    // array2d[0][0] = 14;
-    // array2d[0][1] = 15;
-    // array2d[0][2] = 31;
-    // array2d[0][3] = 20;
-    // array2d[0][4] = 9;
-    // array2d[0][5] = 4;
-    // array2d[0][6] = 29;
-    // array2d[0][7] = 18;
+    // array2d[0][0] = 17;
+    // array2d[0][1] = 25;
+    // array2d[0][2] = 14;
+    // array2d[0][3] = 5;
+    // array2d[0][4] = 31;
+    // array2d[0][5] = 19;
+    // array2d[0][6] = 10;
+    // array2d[0][7] = 28;
+    
+    // array2d[0][8] = 17;
+    // array2d[0][9] = 25;
+    // array2d[0][10] = 14;
+    // array2d[0][11] = 5;
+    // array2d[0][12] = 31;
+    // array2d[0][13] = 19;
+    // array2d[0][14] = 10;
+    // array2d[0][15] = 28;
+
+    // array2d[0][16] = 17;
+    // array2d[0][17] = 25;
+    // array2d[0][18] = 14;
+    // array2d[0][19] = 5;
+    // array2d[0][20] = 31;
+    // array2d[0][21] = 19;
+    // array2d[0][22] = 10;
+    // array2d[0][23] = 28;
+
+    // array2d[0][24] = 17;
+    // array2d[0][25] = 25;
+    // array2d[0][26] = 14;
+    // array2d[0][27] = 5;
+    // array2d[0][28] = 31;
+    // array2d[0][29] = 19;
+    // array2d[0][30] = 10;
+    // array2d[0][31] = 28;
 
     // array2d[0][8] = 0;
     // array2d[0][9] = 0;
@@ -68,6 +95,9 @@ void fill_array_manual(){
 void display_array(){
     for(int i = 0; i < ROW; i++) {
         for(int j = 0; j < COL; j++){
+            if (j % 8 == 0){
+                printf("\t");
+            }
             printf("%d ", array2d[i][j]);
         }
         printf("\n");
@@ -77,6 +107,9 @@ void display_array(){
 void display_array_uncomp(){
     for(int i = 0; i < ROW; i++) {
         for(int j = 0; j < COL; j++){
+            if (j % 8 == 0){
+                printf("\t");
+            }
             printf("%d ", uncompressed_array[i][j]);
         }
         printf("\n");
@@ -84,22 +117,28 @@ void display_array_uncomp(){
 }
 
 void display_array_comp(){
-    for(int i = 0; i < 2; i++) {
-        for(int j = 0; j < 10; j++){
+    for(int i = 0; i < ROW; i++) {
+        for(int j = 0; j < 45; j++){
+            if (j % 5 == 0){
+                printf("\t");
+            }
             printf("%d ", compressed_array[i][j]);
         }
         printf("\n");
     }
 }
 
-void compare_array(){
+int compare_array(){
     for(int i = 0 ; i < ROW ; i++ ) {
         for(int j = 0; j < COL; j++){
             if (array2d[i][j] != uncompressed_array[i][j]){
                 printf("\nERROR!!!\n");
+                return 0;
             }
         }
     }
+    printf("NO ERROR!!!\n");
+    return 1;
 }
 
 void decompress(){
@@ -117,9 +156,6 @@ void decompress(){
             unsigned char element_2_away = (k > 1) ? compressed_array[i][k-2] : 0;
             unsigned char element_3_away = (k > 2) ? compressed_array[i][k-3] : 0;
 
-            // if (count % 8 == 0){
-            //     printf("%d", k);
-            // }
             switch(count){
                 case(0):
                     this_element >>= 3;
@@ -136,7 +172,6 @@ void decompress(){
                     break;
                 
                 case(2):
-                    printf("prev element %d\n", prev_element);
                     prev_element <<= 2;
                     prev_element >>= 3;
                     uncompressed_array[i][j] = prev_element;
@@ -212,11 +247,15 @@ void compress(){
         compressed_byte_counter = 0;
         for(int j = 0; j < COL; j++){
 
+            if(count == 0){
+                j = compressed_byte_counter * 8;
+                compressed_byte_counter ++;
+            }
+
             unsigned int this_element = array2d[i][j];
             unsigned int next_element = (j < COL - 1) ? array2d[i][j+1] : 0;
             unsigned int element_2_away = (j < COL - 2) ? array2d[i][j+2] : 0;
             unsigned int element_3_away = (j < COL - 3) ? array2d[i][j+3] : 0;
-
 
             switch(count){
                 case 0:
@@ -286,8 +325,8 @@ void compress(){
 
             if (count == 5){
                 count = 0;
-                compressed_byte_counter ++;
-                j = compressed_byte_counter * 7;
+                // compressed_byte_counter ++;
+                // j = compressed_byte_counter * 8;
             }
         }
 
@@ -308,15 +347,16 @@ int main(char **argv, int argc){
     // fill_array();
     fill_array_manual();
     display_array();
-    printf("\n\n");
+    printf("\n");
 
     compress();
     display_array_comp();
-    printf("\n\n");
+    printf("\n");
 
 
     decompress();
     display_array_uncomp();
     
     compare_array();
+    return 0;
 }
