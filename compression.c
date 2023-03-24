e5ce6231e82bfa611a5b567ad263d584e5284f94
CJ Coleman <py20cjcb@leeds.ac.uk>
2023-03-24 15:03:15 +0000

    compression function changed to unsigned char - working not working

--- compression.c
+++ compression.c
@@ -3,15 +3,15 @@
 #include<time.h>
 
 
-#define ROW 10
-#define COL 72
+#define ROW 3
+#define COL 40
 
 void print_binary(int num);
 
 
 unsigned int *arr[ROW];
 unsigned int **array2d = arr;
-unsigned int compressed_array[ROW][45] = {{0}};
+unsigned int compressed_array[ROW][25] = {{0}};
 
 unsigned int uncompressed_array[ROW][COL] = {{0}};
 
@@ -27,69 +27,70 @@ void fill_array_manual(){
         for(int j = 0; j < COL; j++)
             array2d[i][j] = rand() % 31;
     }
+
+
+//     array2d[0][0] = 31;
+//     array2d[0][1] = 0;
+//     array2d[0][2] = 1;
+//     array2d[0][3] = 15;
+//     array2d[0][4] = 17;
+//     array2d[0][5] = 12;
+//     array2d[0][6] = 25;
+//     array2d[0][7] = 4;
     
-    // array2d[0][0] = 17;
-    // array2d[0][1] = 25;
-    // array2d[0][2] = 14;
-    // array2d[0][3] = 5;
-    // array2d[0][4] = 31;
-    // array2d[0][5] = 19;
-    // array2d[0][6] = 10;
-    // array2d[0][7] = 28;
+//     array2d[0][8] = 17;
+//     array2d[0][9] = 25;
+//     array2d[0][10] = 14;
+//     array2d[0][11] = 5;
+//     array2d[0][12] = 31;
+//     array2d[0][13] = 19;
+//     array2d[0][14] = 10;
+//     array2d[0][15] = 28;
+
+//     array2d[0][16] = 17;
+//     array2d[0][17] = 25;
+//     array2d[0][18] = 14;
+//     array2d[0][19] = 5;
+//     array2d[0][20] = 31;
+//     array2d[0][21] = 19;
+//     array2d[0][22] = 10;
+//     array2d[0][23] = 28;
+
+//     array2d[0][24] = 17;
+//     array2d[0][25] = 25;
+//     array2d[0][26] = 14;
+//     array2d[0][27] = 5;
+//     array2d[0][28] = 31;
+//     array2d[0][29] = 19;
+//     array2d[0][30] = 10;
+//     array2d[0][31] = 28;
+
+//     // array2d[0][8] = 0;
+//     // array2d[0][9] = 0;
+//     // array2d[0][10] = 1;
+//     // array2d[0][11] = 15;
+//     // array2d[0][12] = 17;
+//     // array2d[0][13] = 12;
+//     // array2d[0][14] = 25;
+//     // array2d[0][15] = 4;
     
-    // array2d[0][8] = 17;
-    // array2d[0][9] = 25;
-    // array2d[0][10] = 14;
-    // array2d[0][11] = 5;
-    // array2d[0][12] = 31;
-    // array2d[0][13] = 19;
-    // array2d[0][14] = 10;
-    // array2d[0][15] = 28;
+//     array2d[1][0] = 0;
+//     array2d[1][1] = 0;
+//     array2d[1][2] = 0;
+//     array2d[1][3] = 0;
+//     array2d[1][4] = 17;
+//     array2d[1][5] = 12;
+//     array2d[1][6] = 25;
+//     array2d[1][7] = 4;
 
-    // array2d[0][16] = 17;
-    // array2d[0][17] = 25;
-    // array2d[0][18] = 14;
-    // array2d[0][19] = 5;
-    // array2d[0][20] = 31;
-    // array2d[0][21] = 19;
-    // array2d[0][22] = 10;
-    // array2d[0][23] = 28;
-
-    // array2d[0][24] = 17;
-    // array2d[0][25] = 25;
-    // array2d[0][26] = 14;
-    // array2d[0][27] = 5;
-    // array2d[0][28] = 31;
-    // array2d[0][29] = 19;
-    // array2d[0][30] = 10;
-    // array2d[0][31] = 28;
-
-    // array2d[0][8] = 0;
-    // array2d[0][9] = 0;
-    // array2d[0][10] = 1;
-    // array2d[0][11] = 15;
-    // array2d[0][12] = 17;
-    // array2d[0][13] = 12;
-    // array2d[0][14] = 25;
-    // array2d[0][15] = 4;
-    
-    // array2d[1][0] = 0;
-    // array2d[1][1] = 0;
-    // array2d[1][2] = 0;
-    // array2d[1][3] = 0;
-    // array2d[1][4] = 17;
-    // array2d[1][5] = 12;
-    // array2d[1][6] = 25;
-    // array2d[1][7] = 4;
-
-    // array2d[1][8] = 31;
-    // array2d[1][9] = 0;
-    // array2d[1][10] = 1;
-    // array2d[1][11] = 15;
-    // array2d[1][12] = 17;
-    // array2d[1][13] = 12;
-    // array2d[1][14] = 25;
-    // array2d[1][15] = 4;
+//     array2d[1][8] = 31;
+//     array2d[1][9] = 0;
+//     array2d[1][10] = 1;
+//     array2d[1][11] = 15;
+//     array2d[1][12] = 17;
+//     array2d[1][13] = 12;
+//     array2d[1][14] = 25;
+//     array2d[1][15] = 4;
 }
 
 void display_array(){
@@ -252,69 +253,66 @@ void compress(){
                 compressed_byte_counter ++;
             }
 
-            unsigned int this_element = array2d[i][j];
-            unsigned int next_element = (j < COL - 1) ? array2d[i][j+1] : 0;
-            unsigned int element_2_away = (j < COL - 2) ? array2d[i][j+2] : 0;
-            unsigned int element_3_away = (j < COL - 3) ? array2d[i][j+3] : 0;
+            unsigned char this_element = array2d[i][j];
+            unsigned char next_element = (j < COL - 1) ? array2d[i][j+1] : 0;
+            unsigned char element_2_away = (j < COL - 2) ? array2d[i][j+2] : 0;
+            unsigned char element_3_away = (j < COL - 3) ? array2d[i][j+3] : 0;
 
             switch(count){
                 case 0:
-                    this_element <<= 27;
-                    this_element >>= 24;
+                    this_element <<= 3;
 
-                    next_element <<= 27;
-                    next_element >>= 29;
+                    next_element <<= 3;
+                    next_element >>= 5;
 
                     this_element |= next_element;
                     compressed_array[i][k] = this_element;
-
                     break;
 
                 case 1:
-                    this_element <<= 30;
-                    this_element >>= 24;
+                    this_element <<= 6;
 
-                    next_element <<= 25;
-                    next_element >>= 24;
+                    next_element <<= 3;
+                    next_element >>= 2;
 
                     this_element |= next_element;
         
-                    element_2_away <<= 27;
-                    element_2_away >>= 31;
+                    element_2_away <<= 3;
+                    element_2_away >>= 7;
 
                     this_element |= element_2_away;
                     compressed_array[i][k] = this_element;
                     break;
 
                 case 2:
-                    //print_binary(next_element);
-                    next_element <<= 28;
-                    next_element >>= 24;
+                    next_element <<= 4;
 
-                    element_2_away <<= 27;
-                    element_2_away >>= 28;
+                    element_2_away <<= 3;
+                    element_2_away >>= 4;
 
                     next_element |= element_2_away;
                     compressed_array[i][k] = next_element;
                     break;
 
                 case 3:
-                    next_element <<= 31;
-                    next_element >>= 24;
+                    next_element <<= 7;
 
-                    element_2_away <<= 26;
-                    element_2_away >>= 24;
+                    element_2_away <<= 3;
+                    element_2_away >>= 1;
 
                     next_element |= element_2_away;
                 
-                    element_3_away >>= 3;
+                    element_3_away <<= 3;
+                    element_3_away >>= 6;
                     next_element |= element_3_away;
                     compressed_array[i][k] = next_element;
                     break;
 
                 case 4:
-                    element_2_away <<= 29;
-                    element_2_away >>= 24;
+                    element_2_away <<= 5;
+
+                    element_3_away <<= 3;
+                    element_3_away >>= 3;
 
                     element_2_away |= element_3_away;
                     compressed_array[i][k] = element_2_away;
@@ -325,8 +323,6 @@ void compress(){
 
             if (count == 5){
                 count = 0;
-                // compressed_byte_counter ++;
-                // j = compressed_byte_counter * 8;
             }
         }
 
