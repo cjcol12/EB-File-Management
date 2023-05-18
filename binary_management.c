1c15768c14005d1836bc84905fc68786b3f16215
CJ Coleman <py20cjcb@leeds.ac.uk>
2023-05-18 18:41:38 +0100

    compression working

--- binary_management.c
+++ binary_management.c
@@ -28,7 +28,6 @@
 
 int read_binary_data(image_struct_type *image_struct, char *input_file_name, FILE *input_file)
 {
-
     unsigned char test;
     fread(&test, sizeof(unsigned char), 1, input_file); // not sure why needed
     // fread starts on the wrong line - manually incremenet it
@@ -153,7 +152,7 @@ void comp(unsigned char buffer[], unsigned char comp_buffer[], FILE *output_file
     }
 }
 
-void compress_data_to_file(image_struct_type *image_struct, FILE *output_file)
+int compress_data_to_file(image_struct_type *image_struct, FILE *output_file)
 {
     unsigned char comp_buffer[5];
     unsigned char buffer[8];
@@ -179,101 +178,6 @@ void compress_data_to_file(image_struct_type *image_struct, FILE *output_file)
 
         // Compress the buffer and write to output file
         comp(buffer, comp_buffer, output_file);
-    }
-}
-
-int compress_data(image_struct_type *image_struct, image_struct_type *image_struct_compressed)
-{
-    int count;
-    int compressed_byte_counter = 0;
-    int k;
-
-    for (int i = 0; i < image_struct_compressed->height; i++)
-    {
-        count = 0;
-        k = 0;
-        compressed_byte_counter = 0;
-        for (int j = 0; j < image_struct->width; j++)
-        {
-
-            unsigned char this_element = image_struct->imageData[i][j];
-            unsigned char next_element = (j < image_struct->width - 1) ? image_struct->imageData[i][j + 1] : 0;
-            unsigned char element_2_away = (j < image_struct->width - 2) ? image_struct->imageData[i][j + 2] : 0;
-            unsigned char element_3_away = (j < image_struct->width - 3) ? image_struct->imageData[i][j + 3] : 0;
-
-            switch (count)
-            {
-            case 0:
-                this_element <<= 3;
-
-                next_element <<= 3;
-                next_element >>= 5;
-
-                this_element |= next_element;
-                image_struct_compressed->imageData[i][k] = this_element;
-
-                break;
-
-            case 1:
-                this_element <<= 6;
-
-                next_element <<= 3;
-                next_element >>= 2;
-
-                this_element |= next_element;
-
-                element_2_away <<= 3;
-                element_2_away >>= 7;
-
-                this_element |= element_2_away;
-
-                image_struct_compressed->imageData[i][k] = this_element;
-                break;
-
-            case 2:
-                next_element <<= 4;
-
-                element_2_away <<= 3;
-                element_2_away >>= 4;
-
-                next_element |= element_2_away;
-                image_struct_compressed->imageData[i][k] = next_element;
-                break;
-
-            case 3:
-                next_element <<= 7;
-
-                element_2_away <<= 3;
-                element_2_away >>= 1;
-
-                next_element |= element_2_away;
-
-                element_3_away <<= 3;
-                element_3_away >>= 6;
-                next_element |= element_3_away;
-                image_struct_compressed->imageData[i][k] = next_element;
-                break;
-
-            case 4:
-                element_2_away <<= 5;
-
-                element_3_away <<= 3;
-                element_3_away >>= 3;
-
-                element_2_away |= element_3_away;
-                image_struct_compressed->imageData[i][k] = element_2_away;
-                break;
-            }
-            count++;
-            k++;
-
-            if (count == 5)
-            {
-                count = 0;
-                compressed_byte_counter++;
-                j = compressed_byte_counter * 7;
-            }
-        }
     }
     return FUNCTION_SUCCESS;
 }
@@ -317,8 +221,6 @@ int read_compressed_data(image_struct_type *image_struct, char *input_file_name,
             // read in binary values
             image_struct->check = fread(&binary_value, sizeof(unsigned char), 1, input_file);
 
-            
-
             if (image_struct->check == 0){
                 return FUNCTION_SUCCESS;
             }
@@ -326,160 +228,15 @@ int read_compressed_data(image_struct_type *image_struct, char *input_file_name,
             value = (unsigned int)binary_value;
             image_struct->imageData[i][j] = value;
         }
-
     }
     fclose(input_file);
     return FUNCTION_SUCCESS;
 }
 
-
-
-
-
-// void decomp(unsigned char buffer[], unsigned char uncomp_buffer[], image_struct_type *image_struct, int count, int i, int k, int j){ //remove j used for debugging
-//     // unsigned char uncomp_buffer[8];
-//     k = 0;
-
-//     for (int l = 0; l < 8; l++){
-
-//         unsigned char this_element = buffer[count];
-//         unsigned char prev_element = (count > 0) ? buffer[count - 1] : 0;
-//         unsigned char element_2_away = (count > 1) ? buffer[count - 2] : 0;
-//         unsigned char element_3_away = (count > 2) ? buffer[count - 3] : 0;
-
-//         switch (count)
-//         {
-//             case (0):
-//                 this_element >>= 3;
-//                 // image_struct->imageData[i][k] = this_element;
-//                 uncomp_buffer[k] = this_element;
-//                 break;
-
-//             case (1):
-//                 prev_element <<= 5;
-//                 prev_element >>= 3;
-
-//                 this_element >>= 6;
-//                 this_element |= prev_element;
-//                 // image_struct->imageData[i][k] = this_element;       
-//                 uncomp_buffer[k] = this_element;
-     
-//                 break;
-
-//             case (2):
-//                 prev_element <<= 2;
-//                 prev_element >>= 3;
-//                 // image_struct->imageData[i][k] = prev_element;
-//                 uncomp_buffer[k] = prev_element;
-
-//                 break;
-
-//             case (3):
-//                 element_2_away <<= 7;
-//                 element_2_away >>= 3;
-
-//                 prev_element >>= 4;
-
-//                 prev_element |= element_2_away;
-//                 // image_struct->imageData[i][k] = prev_element; 
-//                 uncomp_buffer[k] = prev_element;            
-//                 break;
-
-//             case (4):
-//                 element_2_away <<= 4;
-//                 element_2_away >>= 3;
-
-//                 prev_element >>= 7;
-//                 element_2_away |= prev_element;
-//                 // image_struct->imageData[i][k] = element_2_away;   
-//                 uncomp_buffer[k] = element_2_away;           
-//                 break;
-
-//             case (5):
-//                 element_2_away <<= 1;
-//                 element_2_away >>= 3;
-//                 // image_struct->imageData[i][k] = element_2_away;
-//                 uncomp_buffer[k] = element_2_away;
-//                 break;
-
-//             case (6):
-//                 element_3_away <<= 6;
-//                 element_3_away >>= 3;
-
-//                 element_2_away >>= 5;
-
-//                 element_3_away |= element_2_away;
-//                 // image_struct->imageData[i][k] = element_3_away;
-//                 uncomp_buffer[k] = element_3_away;
-//                 break;
-
-//             case (7):
-//                 element_3_away <<= 3;
-//                 element_3_away >>= 3;
-//                 // image_struct->imageData[i][k] = element_3_away;
-//                 uncomp_buffer[k] = element_3_away;
-//                 break;
-//             }
-//             count++;
-//             k++;
-
-//         if (count == 8)
-//         {
-//             count = 0;
-//         }
-//     }
-    
-//     printf("uncomp\t");
-//     for (int l = 0; l < 8; l++){
-//         printf("%d ", uncomp_buffer[l]);
-//     }
-
-//     printf("\n");
-//     for(int z = 0; z < 8; z++){
-//         image_struct->imageData[i][z] = uncomp_buffer[z];
-//     }
-
-//     printf("\n\n");
-
-// }
-
-
-// int decompress_and_store(image_struct_type *image_struct, FILE *input_file)
-// {
-
-//     unsigned char test;
-//     fread(&test, sizeof(unsigned char), 1, input_file); // not sure why needed
-//     // fread starts on the wrong line - manually incremenet it
-
-//     int count = 0;
-//     int k = 0;
-
-//     // Read the image data
-//     unsigned char buffer[5];
-//     unsigned char uncomp_buffer[8];
-
-//     for (int i = 0; i < image_struct->height; i++)
-//     {
-//         k = 0;
-//         count = 0;
-//         for (int j = 0; j < image_struct->compressed_width; j += 5) // width / block size
-//         {
-//             image_struct->check = fread(buffer, 1, 5, input_file);
-            
-//             decomp(buffer, uncomp_buffer, image_struct, count, i, k, j);
-
-//             k += 8;
-//         }
-//     }
-
-//     fclose(input_file);
-//     return FUNCTION_SUCCESS;
-// }
-
-void decomp(unsigned char buffer[], unsigned char uncomp_buffer[], image_struct_type *image_struct, int i, int j, int k){
+void decomp(unsigned char buffer[], unsigned char uncomp_buffer[], image_struct_type *image_struct){
     int local_k = 0;
     int count = 0;
-    unsigned char full_buffer[250];
+    // unsigned char full_buffer[250];
 
     for (int l = 0; l < 8; l++){
 
@@ -492,7 +249,6 @@ void decomp(unsigned char buffer[], unsigned char uncomp_buffer[], image_struct_
         {
             case (0):
                 this_element >>= 3;
-                // image_struct->imageData[i][k] = this_element;
                 uncomp_buffer[local_k] = this_element;
                 break;
 
@@ -502,7 +258,6 @@ void decomp(unsigned char buffer[], unsigned char uncomp_buffer[], image_struct_
 
                 this_element >>= 6;
                 this_element |= prev_element;
-                // image_struct->imageData[i][k] = this_element;       
                 uncomp_buffer[local_k] = this_element;
      
                 break;
@@ -510,7 +265,6 @@ void decomp(unsigned char buffer[], unsigned char uncomp_buffer[], image_struct_
             case (2):
                 prev_element <<= 2;
                 prev_element >>= 3;
-                // image_struct->imageData[i][k] = prev_element;
                 uncomp_buffer[local_k] = prev_element;
 
                 break;
@@ -522,7 +276,6 @@ void decomp(unsigned char buffer[], unsigned char uncomp_buffer[], image_struct_
                 prev_element >>= 4;
 
                 prev_element |= element_2_away;
-                // image_struct->imageData[i][k] = prev_element; 
                 uncomp_buffer[local_k] = prev_element;            
                 break;
 
@@ -532,14 +285,12 @@ void decomp(unsigned char buffer[], unsigned char uncomp_buffer[], image_struct_
 
                 prev_element >>= 7;
                 element_2_away |= prev_element;
-                // image_struct->imageData[i][k] = element_2_away;   
                 uncomp_buffer[local_k] = element_2_away;           
                 break;
 
             case (5):
                 element_2_away <<= 1;
                 element_2_away >>= 3;
-                // image_struct->imageData[i][k] = element_2_away;
                 uncomp_buffer[local_k] = element_2_away;
                 break;
 
@@ -550,14 +301,12 @@ void decomp(unsigned char buffer[], unsigned char uncomp_buffer[], image_struct_
                 element_2_away >>= 5;
 
                 element_3_away |= element_2_away;
-                // image_struct->imageData[i][k] = element_3_away;
                 uncomp_buffer[local_k] = element_3_away;
                 break;
 
             case (7):
                 element_3_away <<= 3;
                 element_3_away >>= 3;
-                // image_struct->imageData[i][k] = element_3_away;
                 uncomp_buffer[local_k] = element_3_away;
                 break;
             }
@@ -569,162 +318,53 @@ void decomp(unsigned char buffer[], unsigned char uncomp_buffer[], image_struct_
             count = 0;
         }
     }
-
- 
-
-
-    // if (i < 3){
-    // for (int l = 0; l < 8; l++){
-    //     printf("%2d ", uncomp_buffer[l]);
-    //     // image_struct->imageData[i][j] = 
-    // }
-    // }
-
 }
 
-// int decompress_and_store(image_struct_type *image_struct, FILE *input_file)
-// {
-//     int flag = 0;
-//     unsigned char test;
-//     fread(&test, sizeof(unsigned char), 1, input_file); // not sure why needed
-//     // fread starts on the wrong line - manually incremenet it
-
-//     // Read the image data
-//     unsigned char buffer[5];
-//     unsigned char uncomp_buffer[8];
-//     for (int i = 0; i < image_struct->height; i++)
-//     {
-//         printf("\n");
-//         for (int j = 0, k = 0; j < image_struct->compressed_width; j += 5) // width / block size
-//         {
-//             image_struct->check = fread(buffer, 1, 5, input_file);
-          
-//             decomp(buffer, uncomp_buffer, image_struct, j);
-
-//             for(int z = 0; z < 8; z++, k++){
-//                 if(k > image_struct->width){
-//                     i++;
-//                     k = 0;
-//                     flag = 1;
-//                 }
-//                 image_struct->imageData[i][k] = uncomp_buffer[z];
-//             }
-
-//         }
-//             if (flag == 1){
-//                 flag = 0;
-//                 i--;
-//             }
-//     }
-
-//     fclose(input_file);
-//     return FUNCTION_SUCCESS;
-// }
-
-int decompress_and_store(image_struct_type *image_struct, FILE *input_file)
+int decompress_and_store(image_struct_type *image_struct, FILE *input_file, char *input_file_name)
 {
-    int flag = 0;
-    int k = 0;
     unsigned char test;
-    unsigned char full_buffer[250];
-
-
     fread(&test, sizeof(unsigned char), 1, input_file); // not sure why needed
     // fread starts on the wrong line - manually incremenet it
+
+    int k = 0;
 
     // Read the image data
     unsigned char buffer[5];
     unsigned char uncomp_buffer[8];
-    for (int i = 0; i < image_struct->height; i++)
+
+    for (int i = 0; i < image_struct->compressed_numBytes; i += 5, k += 3)
     {
-        printf("\n\n");
-        for (int j = 0; j < image_struct->compressed_width; j++) // width / block size
-        {
-            for (int x = 0; x < 5; x++){
-                image_struct->check = fread(&buffer[x], 1, 1, input_file);
+        image_struct->check = fread(buffer, 1, 5, input_file);
+            
+        decomp(buffer, uncomp_buffer, image_struct);
+
+        for(int z = 0; z < 8; z++){
+            if (i + z + k < image_struct->numBytes){
+                image_struct->array[i + z + k] = uncomp_buffer[z];
             }
-
-            decomp(buffer, uncomp_buffer, image_struct, i, j, k);
-
-
-        
-            for (int l = 0; l < 8; l++){
-                full_buffer[l] = uncomp_buffer[l];
-    
-            for(int z = 0; z < 8; z++){
-                printf("%d ", uncomp_buffer[z]);
-
-                if(k > image_struct->width){
-                    i++;
-                    k = 0;
-                }
-                image_struct->imageData[i][k] = uncomp_buffer[z];
-                k++;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                // if(k > image_struct->width){
-                //     i++;
-                //     k = 0;
-                //     flag = 1;
-                // }
-                // image_struct->imageData[i][k] = uncomp_buffer[z];
-            }
-
         }
-            if (flag == 1){
-                flag = 0;
-                i--;
-            }
+    }
+
+    image_struct->check = fread(buffer, 1, 1, input_file);
+
+    if(image_struct->check != 0){
+        printf("ERROR: Bad Data (%s)\n", input_file_name);
+        return BAD_DATA;
     }
 
     fclose(input_file);
     return FUNCTION_SUCCESS;
 }
 
-// int decompress_and_store(image_struct_type *image_struct, FILE *input_file)
-// {
-//     unsigned char test;
-//     fread(&test, sizeof(unsigned char), 1, input_file); // not sure why needed
-//     // fread starts on the wrong line - manually incremenet it
+void one_dim_two_dim(image_struct_type *image_struct){
+// Iterate through the 1D array and copy elements into 2D array
+    for (int i = 0; i < image_struct->height; i++) {
+        for (int j = 0; j < image_struct->width; j++) {
+            // work out the 1D index corresponding to 2D indices
+            int one_d_index = i * image_struct->width + j;
 
-//     int count = 0;
-//     int k = 0;
-
-//     // Read the image data
-//     unsigned char buffer[5];
-//     unsigned char uncomp_buffer[8];
-
-//     for (int i = 0; i < image_struct->height; i++)
-//     {
-//         k = 0;
-//         count = 0;
-//         for (int j = 0; j < image_struct->compressed_width; j += 5) // width / block size
-//         {
-//             image_struct->check = fread(buffer, 1, 5, input_file);
-            
-//             decomp(buffer, uncomp_buffer, image_struct, count, i, k);
-
-//             for(int z = 0; z < 8; z++){
-//                 image_struct->imageData[i][j + z] = uncomp_buffer[z];
-//             }
-
-//             k += 8;
-//         }
-//     }
-
-//     fclose(input_file);
-//     return FUNCTION_SUCCESS;
-// }
-
+            // Copy the element from the 1D array to the 2D array
+            image_struct->imageData[i][j] = image_struct->array[one_d_index];
+        }
+    }
+}
--- binary_management.h
+++ binary_management.h
@@ -5,13 +5,17 @@ int read_binary_data(image_struct_type *image_struct, char *input_file_name, FIL
 
 int write_binary_data(image_struct_type *image_struct, FILE *output_file);
 
-int compress_data(image_struct_type *image_struct, image_struct_type *image_struct_compressed);
+void comp(unsigned char buffer[], unsigned char comp_buffer[], FILE *output_file);
+
+int compress_data_to_file(image_struct_type *image_struct, FILE *output_file);
 
 void round_up(image_struct_type *image_struct, image_struct_type *image_struct_compressed);
 
 int round_up_return(image_struct_type *image_struct);
 
 int read_compressed_data(image_struct_type *image_struct, char *input_file_name, FILE *input_file);
+
+void decomp(unsigned char buffer[], unsigned char uncomp_buffer[], image_struct_type *image_struct);
 
 int decompress(image_struct_type *image_struct, image_struct_type *image_struct_compressed);
 
